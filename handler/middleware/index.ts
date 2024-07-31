612edd86e4a93361b278deb4791a48ef60c988c4
ezecodes <>
2024-07-31 04:33:58 +0100

    update highlights

--- handler/middleware/index.ts
+++ handler/middleware/index.ts
@@ -1,24 +1,42 @@
 import { NextFunction, Response } from "express";
-import helmet from "helmet"
-import {port} from "../config"
+import helmet from "helmet";
+import { port } from "../config";
 
-export default () => helmet({
-  contentSecurityPolicy: {
-    directives: {
-      defaultSrc: ["'self'"],
-      connectSrc: ["'self'"],
-      frameSrc: ["'self'",],
-      styleSrc: ["'self'", "stackpath.bootstrapcdn.com", "'unsafe-inline'", "fonts.googleapis.com", "cdnjs.cloudflare.com"],
-      scriptSrc: ["'self'", "cdn.jsdelivr.net", "unpkg.com"],
-      imgSrc: ["'self'", "upload.wikimedia.org","avatars.githubusercontent.com", "raw.githubusercontent.com", "github.githubassets.com", "docs.amplify.aws/assets/logo-dark.svg", "www.vectorlogo.zone", "cdn.worldvectorlogo.com", "reactnative.dev/img/header_logo.svg"],
+export default () =>
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        connectSrc: ["'self'"],
+        frameSrc: ["'self'"],
+        styleSrc: [
+          "'self'",
+          "stackpath.bootstrapcdn.com",
+          "'unsafe-inline'",
+          "fonts.googleapis.com",
+          "cdnjs.cloudflare.com",
+        ],
+        scriptSrc: ["'self'", "cdn.jsdelivr.net", "unpkg.com"],
+        imgSrc: [
+          "'self'",
+          "https://miro.medium.com/v2/resize:fit:700/0*IF5Z05nkdvA3JDXI.png",
+          "upload.wikimedia.org",
+          "avatars.githubusercontent.com",
+          "raw.githubusercontent.com",
+          "github.githubassets.com",
+          "docs.amplify.aws/assets/logo-dark.svg",
+          "www.vectorlogo.zone",
+          "cdn.worldvectorlogo.com",
+          "reactnative.dev/img/header_logo.svg",
+        ],
+      },
     },
-  },
-  frameguard: {
-    action: "deny",
-  },
-  noSniff: true,
-})
- 
+    frameguard: {
+      action: "deny",
+    },
+    noSniff: true,
+  });
+
 export function onError(error: any | Error): void {
   if (error.syscall !== "listen") {
     throw error;
@@ -39,11 +57,15 @@ export function onError(error: any | Error): void {
   }
 }
 
-export const serverHeaders = (req: Request, res: Response, next: NextFunction) => {
+export const serverHeaders = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   res.setHeader("X-Content-Type-Options", "nosniff");
   res.setHeader("X-Frame-Options", "DENY");
   res.setHeader("X-Powered-By", "");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("X-XSS-Protection", "1; mode=block");
-  next()
-}
+  next();
+};
